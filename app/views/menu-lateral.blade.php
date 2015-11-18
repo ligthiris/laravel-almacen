28b7e2ce8a8d16996e9483e325b59f58f3fdfd43
Daniel Salas <d.salas.denegri@isur.edu.pe>
2015-11-18 18:11:03 -0500

    categorias y secciones de plantilla

---
+++ app/views/menu-lateral.blade.php
@@ -0,0 +1,20 @@
+<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="/"><i class="fa fa-fw fa-home"></i> Inicio</a>
+        </li>
+        <li>
+            <a href="/categorias"><i class="fa fa-fw fa-tag"></i> Categorías</a>
+        </li>
+        <li>
+            <a href="/productos"><i class="fa fa-fw fa-cube"></i> Productos</a>
+        </li>
+        <li>
+            <a href="orders.html"><i class="fa fa-fw fa-file-text"></i> Pedidos</a>
+        </li>
+        <li>
+            <a href="users.html"><i class="fa fa-fw fa-user"></i> Usuarios</a>
+        </li>
+    </ul>
+</div>
---
+++ app/views/test.blade.php
@@ -0,0 +1,7 @@
+@extends('template')
+
+@section('contenido')
+
+    <h1>Prueba</h1>
+
+@endsection
